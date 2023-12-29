b1a9ab2d5418ae9cf12be425ca90d70e671086d6
TuriansNotBad <54774532+TuriansNotBad@users.noreply.github.com>
2023-12-29 16:12:27 +0500

    Fix gossip quest lists broken after 3776388.

--- src/game/GossipDef.cpp
+++ src/game/GossipDef.cpp
@@ -488,7 +488,7 @@ void PlayerMenu::SendQuestGiverQuestList(QEmote eEmote, std::string const& Title
             }
 
             data << uint32(questID);
-            data << qmi.m_qIcon;
+            data << uint32(qmi.m_qIcon);
             data << uint32(pQuest->GetQuestLevel());
             data.append(title, titleLen + 1);
         }
