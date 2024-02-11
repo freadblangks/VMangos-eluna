7687b63e5559beb3000a3a4b9532cbc7eb8075bc
Niam5 <ebakkedahl@gmail.com>
2024-02-11 15:20:56 -0600

    Add Eluna LUA engine
    -Add Eluna submodule
    -Add LUA dependencies
    -Update CI build to include Eluna
    
    Thanks to the co-authors who came before and helped get this started.
    
    Co-Authored-By: Vale the Violet Mote <9837348+valethevioletmote@users.noreply.github.com>
    Co-Authored-By: coolzoom <10232727+coolzoom@users.noreply.github.com>
    Co-Authored-By: sifang <bing2008@126.com>

--- src/game/Chat/Chat.cpp
+++ src/game/Chat/Chat.cpp
@@ -39,6 +39,9 @@
 #include "PoolManager.h"
 #include "GameEventMgr.h"
 
+#ifdef ENABLE_ELUNA
+#include "LuaEngine.h"
+#endif /* ENABLE_ELUNA */
 // Supported shift-links (client generated and server side)
 // |color|Harea:area_id|h[name]|h|r
 // |color|Hareatrigger:id|h[name]|h|r
@@ -1861,6 +1864,8 @@ ChatCommandSearchResult ChatHandler::FindCommand(ChatCommand* table, char const*
  */
 void ChatHandler::ExecuteCommand(char const* text)
 {
+    std::string fullcmd = text;
+
     ChatCommand* command = nullptr;
     ChatCommand* parentCommand = nullptr;
 
@@ -1944,6 +1949,10 @@ void ChatHandler::ExecuteCommand(char const* text)
         }
         case CHAT_COMMAND_UNKNOWN_SUBCOMMAND:
         {
+#ifdef ENABLE_ELUNA
+            if (!sEluna->OnCommand(m_session ? m_session->GetPlayer() : NULL, fullcmd.c_str()))
+                return;
+#endif /* ENABLE_ELUNA */
             SendSysMessage(LANG_NO_SUBCMD);
             ShowHelpForCommand(command->ChildCommands, text);
             SetSentErrorMessage(true);
@@ -1951,6 +1960,10 @@ void ChatHandler::ExecuteCommand(char const* text)
         }
         case CHAT_COMMAND_UNKNOWN:
         {
+#ifdef ENABLE_ELUNA
+            if (!sEluna->OnCommand(m_session ? m_session->GetPlayer() : NULL, fullcmd.c_str()))
+                return;
+#endif /* ENABLE_ELUNA */
             SendSysMessage(LANG_NO_CMD);
             SetSentErrorMessage(true);
             break;
