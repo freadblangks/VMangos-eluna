0c4da06813ac796d788cfab5ed64ed320065048c
Niam5 <ebakkedahl@gmail.com>
2024-02-25 16:45:14 -0800

    Eluna multistate update
    -This update allows the use of the new Eluna multistate functionality. Some scripts may break with this please refer to the Eluna API for changes.

--- src/game/Objects/Unit.cpp
+++ src/game/Objects/Unit.cpp
@@ -1072,15 +1072,13 @@ void Unit::Kill(Unit* pVictim, SpellEntry const* spellProto, bool durabilityLoss
     {
         if( pPlayerTap != pPlayerVictim )
         {
-            sEluna->OnPVPKill(pPlayerTap, pPlayerVictim);
+            if (Eluna* e = pPlayerTap->GetEluna())
+                e->OnPVPKill(pPlayerTap, pPlayerVictim);
         }
         //else
         //{
             //sEluna->OnKillSelf(pPlayerVictim);
         //}
-    }else if(pCreatureVictim && pPlayerTap)
-    {
-        sEluna->OnCreatureKill(pPlayerTap, pCreatureVictim);
     }
 #endif /* ENABLE_ELUNA */
 
@@ -1185,6 +1183,14 @@ void Unit::Kill(Unit* pVictim, SpellEntry const* spellProto, bool durabilityLoss
             // durability lost message
             WorldPacket data(SMSG_DURABILITY_DAMAGE_DEATH, 0);
             pPlayerVictim->GetSession()->SendPacket(&data);
+
+#ifdef ENABLE_ELUNA
+            // used by eluna
+            if (Creature* killer = ToCreature())
+                if (pPlayerVictim)
+                    if (Eluna* e = pPlayerVictim->GetEluna())
+                        e->OnPlayerKilledByCreature(killer, pPlayerVictim);
+#endif /* ENABLE_ELUNA */
         }
     }
     else                                                // creature died
@@ -1268,16 +1274,15 @@ void Unit::Kill(Unit* pVictim, SpellEntry const* spellProto, bool durabilityLoss
                 bg->HandleKillUnit(pCreatureVictim, pPlayerTap);
     }
 
-    pVictim->InterruptSpellsCastedOnMe(false, true);
-    
-    // Used by Eluna
 #ifdef ENABLE_ELUNA
-    if (Creature* killer = ToCreature())
+    if (pCreatureVictim && pPlayerTap)
     {
-        if(pPlayerVictim)
-            sEluna->OnPlayerKilledByCreature(killer, pPlayerVictim);
+        if (Eluna* e = pPlayerTap->GetEluna())
+            e->OnCreatureKill(pPlayerTap, pCreatureVictim);
     }
-#endif /* ENABLE_ELUNA */
+#endif
+
+    pVictim->InterruptSpellsCastedOnMe(false, true);
 }
 
 struct PetOwnerKilledUnitHelper
@@ -6228,8 +6233,10 @@ void Unit::SetInCombatState(uint32 combatTimer, Unit* pEnemy)
 
     // Used by Eluna
 #ifdef ENABLE_ELUNA
+     // used by eluna
     if (GetTypeId() == TYPEID_PLAYER)
-        sEluna->OnPlayerEnterCombat(ToPlayer(), pEnemy);
+        if (Eluna* e = ToPlayer()->GetEluna())
+            e->OnPlayerEnterCombat(ToPlayer(), pEnemy);
 #endif /* ENABLE_ELUNA */
 
 }
@@ -6391,7 +6398,8 @@ void Unit::ClearInCombat()
 	// Used by Eluna
 #ifdef ENABLE_ELUNA
     if (GetTypeId() == TYPEID_PLAYER)
-        sEluna->OnPlayerLeaveCombat(ToPlayer());
+        if (Eluna* e = ToPlayer()->GetEluna())
+            e->OnPlayerLeaveCombat(ToPlayer());
 #endif /* ENABLE_ELUNA */
     }
 }
--- src/game/Spells/SpellEffects.cpp
+++ src/game/Spells/SpellEffects.cpp
@@ -2395,10 +2395,12 @@ void Spell::EffectSummon(SpellEffectIndex effIdx)
 
 #ifdef ENABLE_ELUNA
     if (Unit* summoner = m_caster->ToUnit())
-        sEluna->OnSummoned(spawnCreature, summoner);
+        if (Eluna* e = summoner->GetEluna())
+            e->OnSummoned(spawnCreature, summoner);
     else if (m_originalCaster)
         if (Unit* summoner = m_originalCaster->ToUnit())
-            sEluna->OnSummoned(spawnCreature, summoner);
+            if (Eluna* e = summoner->GetEluna())
+                e->OnSummoned(spawnCreature, summoner);
 #endif /* ENABLE_ELUNA */
 }
 
@@ -2735,7 +2737,8 @@ void Spell::EffectSummonWild(SpellEffectIndex effIdx)
 #ifdef ENABLE_ELUNA
             if (m_originalCaster)
                 if (Unit* summoner = m_originalCaster->ToUnit())
-                    sEluna->OnSummoned(summon, summoner);
+                    if (Eluna* e = summoner->GetEluna())
+                        e->OnSummoned(summon, summoner);
 #endif /* ENABLE_ELUNA */
 
 
@@ -2884,10 +2887,12 @@ void Spell::EffectSummonGuardian(SpellEffectIndex effIdx)
 
 #ifdef ENABLE_ELUNA
         if (Unit* summoner = m_caster->ToUnit())
-            sEluna->OnSummoned(spawnCreature, summoner);
+            if (Eluna* e = summoner->GetEluna())
+                e->OnSummoned(spawnCreature, summoner);
         if (m_originalCaster)
             if (Unit* summoner = m_originalCaster->ToUnit())
-                sEluna->OnSummoned(spawnCreature, summoner);
+                if (Eluna* e = summoner->GetEluna())
+                    e->OnSummoned(spawnCreature, summoner);
 #endif /* ENABLE_ELUNA */
 
         if (count == 0)
@@ -2922,7 +2927,8 @@ void Spell::EffectSummonPossessed(SpellEffectIndex effIdx)
 
 #ifdef ENABLE_ELUNA
     if (Unit* summoner = m_originalCaster->ToUnit())
-        sEluna->OnSummoned(pMinion, summoner);
+        if (Eluna* e = summoner->GetEluna())
+            e->OnSummoned(pMinion, summoner);
 #endif /* ENABLE_ELUNA */
 }
 
@@ -4731,7 +4737,8 @@ void Spell::EffectDuel(SpellEffectIndex effIdx)
 
     // Used by Eluna
 #ifdef ENABLE_ELUNA
-    sEluna->OnDuelRequest(target, caster);
+    if (Eluna* e = caster->GetEluna())
+        e->OnDuelRequest(target, caster);
 #endif /* ENABLE_ELUNA */
 }
 
@@ -5450,10 +5457,12 @@ void Spell::EffectSummonCritter(SpellEffectIndex effIdx)
 
 #ifdef ENABLE_ELUNA
     if (Unit* summoner = m_caster->ToUnit())
-        sEluna->OnSummoned(critter, summoner);
+        if (Eluna* e = summoner->GetEluna())
+            e->OnSummoned(critter, summoner);
     if (m_originalCaster)
         if (Unit* summoner = m_originalCaster->ToUnit())
-            sEluna->OnSummoned(critter, summoner);
+            if (Eluna* e = summoner->GetEluna())
+                e->OnSummoned(critter, summoner);
 #endif /* ENABLE_ELUNA */
 }
 
