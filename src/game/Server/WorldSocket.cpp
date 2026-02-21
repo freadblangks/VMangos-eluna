ericssm/Eluna-VMaNGOS

Eluna multistate update
-This update allows the use of the new Eluna multistate functionality. Some scripts may break with this please refer to the Eluna API for changes.

src/game/Protocol/WorldSocket.cpp
@@ -70,7 +70,7 @@ int WorldSocket::ProcessIncoming(WorldPacket* new_pct)
                     return -1;
                 }
 #ifdef ENABLE_ELUNA
-                if (!sEluna->OnPacketReceive(m_Session, *new_pct))
+                if (!sWorld.GetEluna()->OnPacketReceive(m_Session, *new_pct))
                     return 0;
 #endif /* ENABLE_ELUNA */
 

freadblangks/VMangos-eluna

src/game/Server/WorldSocket.cpp
@@ -182,7 +182,7 @@ WorldSocket::HandlerResult WorldSocket::_HandleCompleteReceivedPacket(std::uniqu
                 }
 
 #ifdef ENABLE_ELUNA
-                if (!sEluna->OnPacketReceive(m_Session, *packet))
+                if (!sWorld.GetEluna()->OnPacketReceive(m_Session, *packet))
                     return 0;
 #endif /* ENABLE_ELUNA */
 